ab5e2ceec009b9fc246a6410ded35223bc5df9f8
YoussefKhateeb <145559686+YoussefKhateeb@users.noreply.github.com>
2023-11-28 23:02:04 +0200

    Update Morse Code Translator.py

--- Morse Code Translator.py
+++ Morse Code Translator.py
@@ -11,4 +11,13 @@ MORSE_CODE_DICT = {'A': '.-', 'B': '-...',
                    ',': '--..--', '.': '.-.-.-',
                    '?': '..--..', '/': '-..-.', '-': '-....-',
                    '(': '-.--.', ')': '-.--.', ' ': '/'}
-
+def text_to_morse(text):
+    morse_code = ''
+    for char in text.upper():
+        if char not in MORSE_CODE_DICT:
+            raise ValueError(f"Invalid character '{char}' in the input. Please enter letters in the text.")
+        elif char == ' ':
+            morse_code += '/'
+        else:
+            morse_code += MORSE_CODE_DICT[char] + ' '
+    return morse_code.strip()  
