0f25a6452163a65e1fde81a78d306c42482453cf
YoussefKhateeb <145559686+YoussefKhateeb@users.noreply.github.com>
2023-11-29 19:16:24 +0200

    Update Morse Code Translator.py

--- Morse Code Translator.py
+++ Morse Code Translator.py
@@ -1,4 +1,3 @@
-
 MORSE_CODE_DICT = {'A': '.-', 'B': '-...',
                    'C': '-.-.', 'D': '-..', 'E': '.',
                    'F': '..-.', 'G': '--.', 'H': '....',
@@ -11,26 +10,42 @@ MORSE_CODE_DICT = {'A': '.-', 'B': '-...',
                    ',': '--..--', '.': '.-.-.-',
                    '?': '..--..', '/': '-..-.', '-': '-....-',
                    '(': '-.--.', ')': '-.--.', ' ': '/'}
+
 def text_to_morse(text):
     morse_code = ''
     for char in text.upper():
-        if char not in MORSE_CODE_DICT:
-            raise ValueError(f"Invalid character '{char}' in the input. Please enter letters in the text.")
-        elif char == ' ':
-            morse_code += '/'
-        else:
+        if char == ' ':
+            morse_code += ' '
+        elif char in MORSE_CODE_DICT:
             morse_code += MORSE_CODE_DICT[char] + ' '
-    return morse_code.strip()  
+        else:
+            raise ValueError(f"Invalid character '{char}' in the input. Please enter letters not numbers in the text.")
+    return morse_code.strip()
+
 def morse_to_text(morse_code):
     morse_code = morse_code.split(' ')
     text = ''
     for code in morse_code:
-        if code in MORSE_CODE_DICT.values():
+        if code == '':
+            text += ' '  
+        elif code in MORSE_CODE_DICT.values():
             for key, value in MORSE_CODE_DICT.items():
                 if code == value:
                     text += key
-        elif code == '/':
-            text += ' '
         else:
             raise ValueError(f"Invalid Morse code '{code}' in the input. Please enter valid Morse code.")
     return text
+
+user_input = input("Enter a English text to translate to Morse Code : ")
+try:
+    morse_result = text_to_morse(user_input)
+    print(f'Text to Morse Code: {morse_result}')
+except ValueError as e:
+    print(e)
+
+morse_input = input("Enter Morse code for translation: ")
+try:
+    text_result = morse_to_text(morse_input)
+    print(f'Morse Code to Text: {text_result}')
+except ValueError as e:
+    print(e)
